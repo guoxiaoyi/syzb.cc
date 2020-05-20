6d653add96d4edd23f0ae569fd0352ae92a20a0f
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-05-21 00:10:20 +0800

    锁定gemfile

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,5 @@
+class ContactController < ApplicationController
+  def index
+    @page = WizcmsArticle::Page
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :books
   resources :corpvideo
   resources :news
+  resources :contact
 
 
 
