749be5b74f85448ad09f0f1c3adcb67be46d0c26
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-05-21 14:38:24 +0800

    首页幻灯

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,15 +11,5 @@
 // about supported directives.
 //= require jquery
 //= require swiper.min.js
-//= require carouselbox.min.js
 //= require jquery_ujs
 //
-$(document).ready(function() {
-  $(".e_box.p_problemCategory .e_btn.btn.p_btn-show").click(function(event) {
-    /* Act on the event */
-    $(".e_box.p_category").addClass('animate');
-  });
-  $(".e_btn.p_btn-hide.background_assist").click(function(){
-    $(".e_box.p_category").removeClass('animate');
-  })
-});
