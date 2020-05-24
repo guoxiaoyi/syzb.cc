f01147b153b06c81e2dc1a94d4c74132de1c4947
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-05-24 11:42:39 +0800

    书籍页面导航

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,12 @@
 //= require swiper.min.js
 //= require jquery_ujs
 //
+$(document).ready(function() {
+  $(".e_box.p_problemCategory .e_btn.btn.p_btn-show").click(function(event) {
+    /* Act on the event */
+    $(".e_box.p_category").addClass('animate');
+  });
+  $(".e_btn.p_btn-hide.background_assist").click(function(){
+    $(".e_box.p_category").removeClass('animate');
+  })
+});
