e0f56c170c9854e47df303193ee59fa8acc01a93
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-05-24 15:46:01 +0800

    分页

--- app/controllers/corpvideo_controller.rb
+++ app/controllers/corpvideo_controller.rb
@@ -1,6 +1,6 @@
 class CorpvideoController < ApplicationController
   def index
-    @videos = WizcmsLink::Category.get(:video).links
+    @videos = WizcmsLink::Category.get(:video).links.page( params[:page] ).per(9)
   end
   def show
     @video = WizcmsLink::Link.find( params[:id] )
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,9 +2,9 @@ class NewsController < ApplicationController
   def index
     @article_categories =  WizcmsArticle::ArticleCategory.get(:news)
     @articles = if params[:cid].blank?
-      @article_categories.get_articles_with_chidren
+      @article_categories.get_articles_with_chidren.page( params[:page] ).per(10)
     else
-      @article_categories.children.find(params[:cid]).get_articles
+      @article_categories.children.find(params[:cid]).get_articles.page( params[:page] ).per(10)
     end
 
   end
