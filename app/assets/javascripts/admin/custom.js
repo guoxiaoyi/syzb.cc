609be820ba33fd01c023fdf66db696192b0d8669
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-05-24 16:31:50 +0800

    上传不过滤html

--- app/assets/javascripts/admin/custom.js
+++ app/assets/javascripts/admin/custom.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
-    $('#user_expired_at, #user_birthday').daterangepicker(
-        {
-            locale: 'zh-cn',
-            singleDatePicker: true
-        }
-    )
+// $(document).ready(function () {
+//     $('#user_expired_at, #user_birthday').daterangepicker(
+//         {
+//             locale: 'zh-cn',
+//             singleDatePicker: true
+//         }
+//     )
 
-})
+// })
