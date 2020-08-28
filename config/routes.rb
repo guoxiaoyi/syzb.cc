4a19a89a70feb799d5c850e80b6dca5f2cd6d76b
Guoxiaoyi <guoxiaoyi888@gmail.com>
2020-08-28 10:58:58 +0800

    文字修改

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'contact' => 'contact#index'
+
   get 'api/get_price1'
 
   get 'api/get_switch'
 
   root 'home#index'
 
-  get 'home/index'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
